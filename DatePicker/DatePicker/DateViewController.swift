8cca170f2e6552b646c44357110fb1c52cdb39bf
HyunTaek5 <oht222@naver.com>
2021-03-08 01:32:52 +0900

    [update] tabControl app End

---
+++ DatePicker/DatePicker/DateViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  DatePicker
+//
+//  Created by 오현택 on 2021/02/28.
+//
+
+import UIKit
+
+class DateViewController: UIViewController {
+    let timeSelector: Selector = #selector(DateViewController.updateTime)
+    let interval = 1.0
+    var count = 0
+    
+    @IBOutlet var nowTime: UILabel!
+    @IBOutlet var selectedTime: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
+        
+    }
+    @IBAction func selectTime(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        selectedTime.text = "선택 시간:" + formatter.string(from: datePickerView.date)
+    }
+    @objc func updateTime(){
+        let date = NSDate()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        nowTime.text = "현재 시간:" + formatter.string(from: date as Date)
+    }
+
+}
+
