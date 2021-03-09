57f2e746e4ef7966fc30624543a54ab44c46d2bc
HyunTaek5 <oht222@naver.com>
2021-03-09 22:50:03 +0900

    [update] Navigation app update

--- Navigation/Navigation/ViewController.swift
+++ Navigation/Navigation/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, EditDelegate {
+    
+    
+    @IBOutlet var txtMessage: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let editViewController = segue.destination as! EditViewController
+        if segue.identifier == "editButton" {
+            editViewController.textWayValue = "segue : use button"
+        } else if segue.identifier == "editBarButton" {
+            editViewController.textWayValue = "segue : use Bar button"
+        }
+        editViewController.textMessage = txtMessage.text!
+        editViewController.delegate = self
+    }
+    func didMessageEditDone(_ controller: EditViewController, message: String) {
+        txtMessage.text = message
+    }
+    
 }
 
